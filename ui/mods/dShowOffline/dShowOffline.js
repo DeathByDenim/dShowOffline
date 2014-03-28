784279b43b9ccc213e6cedc23a32ca71a8b9e8f9
DeathByDenim <jarno@jarno.ca>
2014-03-27 20:50:28 -0400

    Only show offline. Don't bother with online.

--- ui/mods/dShowOffline/dShowOffline.js
+++ ui/mods/dShowOffline/dShowOffline.js
@@ -1,16 +1,10 @@
 (function() {
 
-	var oldarmy_state = handlers.army_state;
-	handlers.army_state = function (armies) {
-		oldarmy_state(armies);
-		console.log("dShowOffline:", armies);
-	}
-
-	$('.div_player_name_status').append('<span class="div_player_name ai" data-bind="text: disconnected ? \'OFFLINE\' : \'ONLINE\'"></span>');
+	$('.div_player_name_status').append('<span class="div_player_name ai" data-bind="visible: disconnected">OFFLINE</span>');
 
 	$('.div_spectate_item').each( function(index, item) {
 		console.log('item=',item);
-		$(this).children(":first").append('<span class="div_player_name ai" data-bind="text: disconnected ? \'OFFLINE\' : \'ONLINE\'"></span>');
+		$(this).children(":first").append('<span class="div_player_name ai" data-bind="visible: disconnected">OFFLINE</span>');
 	});
 
 })();
